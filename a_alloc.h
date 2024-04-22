d8583069d4c003882bf5f22fb04569820e4348af
TheCalculus <jaisalbhasinis@gmail.com>
2024-04-22 18:48:38 +1000

    first

---
+++ a_alloc.h
@@ -0,0 +1,14 @@
+#ifndef AALLOC_H
+#define AALLOC_H
+
+#include <stdlib.h>
+
+extern void*  ptrs[512];
+extern size_t position;
+
+void a_malloc(void** ptr, size_t size);
+void a_calloc(void** ptr, size_t nmemb, size_t size);
+void a_manrec(void** ptr);
+void a_freeall();
+
+#endif
